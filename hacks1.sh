99d99c4d6ab088e94c8ae7a25a621ad2a33c012f
Benjamin Lindqvist <benjaminlindqvist@gmail.com>
2018-03-07 23:29:48 +0100

    Some more useful hacks!

--- hacks1.sh
+++ hacks1.sh
@@ -7,7 +7,6 @@
 # Note: Some of these aren't all that useful and are mostly there to showcase
 # random neat stuff. But some of them are pretty golden!
 
-
 random_string()
 # A random "normal" string of length $1 (useful for file names and stuff where
 # you might not want spaces or escape characters)
@@ -23,12 +22,38 @@ random_file()
     head -c "$2" < /dev/urandom > "$1"
 }
 
+require()
+# Give up early rather than late! (this one's actually amazing)
+# Note: you can actually require require! I.e. command -v works for binaries,
+# shell built-ins, aliases, everything.
+{
+    # Usage:  require $app1 $app2 $app3 ...
+    for cmd in "$@" ; do
+        command -v "$cmd" > /dev/null || {
+            errcho "$cmd not found!"
+            return 1
+        }
+    done
+}
+
 copypwd()
 # If you don't have a terminal that lets you navigate through stdout, this one's
 # golden. But also, check out termite.
 {
     require xclip
     pwd | xclip -selection clipboard
+}
+
+file_size()
+{
+    #Usage:  file_size $filename
+    stat -c %s "$1"
+}
+
+hex_to_dec()
+{
+    # Usage:  hex_to_dec [0x]DEADBEEF
+    printf "%d" "$1"
 }
 
 local_ip_with_mask()
@@ -48,20 +73,6 @@ errcho()
     echo "$*" 1>&2
 }
 
-require()
-# Give up early rather than late! (this one's actually amazing)
-# Note: you can actually require require! I.e. command -v works for binaries,
-# shell built-ins, aliases, everything.
-{
-    # Usage:  require $app1 $app2 $app3 ...
-    for cmd in "$@" ; do
-        command -v "$cmd" > /dev/null || {
-            errcho "$cmd not found!"
-            exit 1
-        }
-    done
-}
-
 justfuckingCOMPILEALREADY()
 # When you'd really rather prefer the Linux kernel to make the decisions for you
 {
@@ -74,11 +85,45 @@ mountie()
 # Because typing mount commands is really boring
 {
     # Usage:  mountie [$dev]
+    require sudo
+
     mpoint=/mnt/"$1"
     sudo mkdir -p "$mpoint"
 
     # Ensure moint point is empty, then mount (this works because ls $dir/*
     # errors out if $dir is empty -- mm, tasty hacks...)
-    ls "$mpoint/*" > /dev/null 2>&1 && \
-        sudo mount /dev/"$1" "$mpoint" && echo "Mounted on $mpoint"
+    ls "$mpoint/*" > /dev/null 2>&1 || {
+        errcho "Mount point $mpoint not empty!"
+        return 1
+    }
+    sudo mount /dev/"$1" "$mpoint" && echo "Mounted on $mpoint"
+}
+
+gfind()
+# Agh where did I put it
+{
+    # Usage:  gfind $filename
+    find . | grep "$1" 2>/dev/null
+}
+
+ghist()
+# Ctrl-R:s big brother!
+{
+    # Usage: ghist $cmd_phrase
+    history | grep "$*"
+    echo "Note: !!2034 will execute command #2034 from above"
+}
+
+subdirs()
+# List only subdirs
+{
+    ls "$@" -d */
+}
+
+pid_alive()
+# For pinging processes -- errors out if $pid isn't alive and does nothing at
+# all if it is.
+{
+    # Usage:  pid_alive $pidlist || errcho "uh-oh"
+    kill -s 0 "$@"
 }
